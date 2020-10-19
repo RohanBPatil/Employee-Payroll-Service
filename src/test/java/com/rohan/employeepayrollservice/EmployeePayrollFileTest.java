b35656b41491093d598eda389667d623bc93a1f7
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-19 23:42:53 +0530

    Added : Functionality to count entries

--- src/test/java/com/rohan/employeepayrollservice/EmployeePayrollFileTest.java
+++ src/test/java/com/rohan/employeepayrollservice/EmployeePayrollFileTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 
 class EmployeePayrollFileTest {
 
+	/**
+	 * checking if employees are added to file
+	 */
 	@Test
 	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
 		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000),
@@ -18,6 +21,21 @@ class EmployeePayrollFileTest {
 		long entries = employeeService.countEntries(EmployeePayrollService.IOService.FILE_IO);
 		employeeService.printData(EmployeePayrollService.IOService.FILE_IO);
 		assertEquals(3, entries);
+	}
+
+	/**
+	 * checking if counting number of entries correctly or not
+	 */
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000),
+				new EmployeePayrollData(2, "Bill Gates", 200000),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService employeeService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeeService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+		employeeService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long result = employeeService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
 
 	}
 
