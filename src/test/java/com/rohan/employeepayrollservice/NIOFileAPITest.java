7435140167e81c1c1b2a0a8c3fa34ec2cb01cbef
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-19 22:03:36 +0530

    Added : Employee watch service

--- src/test/java/com/rohan/employeepayrollservice/NIOFileAPITest.java
+++ src/test/java/com/rohan/employeepayrollservice/NIOFileAPITest.java
@@ -49,4 +49,11 @@ class NIOFileAPITest {
 				.forEach(System.out::println);
 	}
 
+	@Test
+	public void GivenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+	}
+
 }
